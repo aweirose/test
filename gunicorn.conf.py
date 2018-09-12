082216b4fe0c0a5a54765d1c0304fd6d8a3c23e6
陈启蔚 <cqw@chenqiweideMac-mini.local>
2018-09-12 10:53:28 +0800

    九月十二2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = '127.0.0.1:8899'
 workers = 3
-errorlog = '/myprojects/test/testlogs/gunicorn.error.log'
-accesslog = '/myprojects/test/testlogs/gunicorn.access.log'
+errorlog = '/myprojects/testlogs/gunicorn.error.log'
+accesslog = '/myprojects/testlogs/gunicorn.access.log'
 loglevel = 'debug'
